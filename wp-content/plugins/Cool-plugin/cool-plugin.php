d19d7a09f041694d998ca43789462200ac57f690
Sunanda Wasala <sunanda@listudiosl.com>
2023-03-31 13:18:36 +0530

    changes

---
+++ wp-content/plugins/Cool-plugin/cool-plugin.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: Cool Plugin
+ * Plugin URI: https://github.com/wppusher/Cool-Plugin
+ * Description: OMG this plugin is so cool.
+ * Version: 5.6
+ * Author: WP Pusher
+ * Author URI: http://wppusher.com
+ * License: GNU GENERAL PUBLIC LICENSE
+ */
+
+// Add a change here
+
+// Add another change from the fork
+
+// Fix the bug
