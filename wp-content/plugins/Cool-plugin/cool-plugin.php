90b23bbd1ee77ef6e4a4cb7a5b814bb57b8a33a9
sunanda wasala <48275082+wasalas893@users.noreply.github.com>
2023-03-31 13:39:53 +0530

    Update cool-plugin.php
    chnages text

--- wp-content/plugins/Cool-plugin/cool-plugin.php
+++ wp-content/plugins/Cool-plugin/cool-plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name: Cool Plugin
  * Plugin URI: https://github.com/wppusher/Cool-Plugin
  * Description: OMG this plugin is so cool.
- * Version: 5.908
+ * Version: 9.99
  * Author: WP Pusher
  * Author URI: http://wppusher.com
  * License: GNU GENERAL PUBLIC LICENSE
